0a17438a80a857af7bf84ec6dc3addac494f57ac
amir mahdi afshar <amirmahdiafshar8@gmail.com>
2025-10-22 05:33:41 -0700

    users.py

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, Depends
-from fastapi import APIRouter, Request, Depends
+from fastapi import APIRouter, Request, Depends,HTTPException
 from fastapi.security import HTTPAuthorizationCredentials
 import jwt
 from app.utils import rate_limiter, get_current_key
@@ -12,6 +11,11 @@ router = APIRouter(
 
 @router.get("/protected", dependencies=[Depends(rate_limiter())])
 def protected(request: Request, credentials: HTTPAuthorizationCredentials = Depends()):
-    _, Skey = get_current_key()
-    payload = jwt.decode(credentials.credentials, Skey, algorithms=[ALGORITHM])
-    return {"message": f"Hello {payload['sub']}, token valid!"}
+    try:
+        _, Skey = get_current_key()
+        payload = jwt.decode(credentials.credentials, Skey, algorithms=[ALGORITHM])
+        return {"message": f"Hello {payload['sub']}, token valid!"}
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token expired")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid token")
